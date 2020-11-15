458e1b2814f184f6009e7bf939ba4274d1ba5228
izaanjahangir <izaanjahangir2@gmail.com>
2020-11-16 01:36:00 +0500

    initial commit

---
+++ LearningAPI.java
@@ -0,0 +1,40 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class LearningAPI {
+    public static void main(String[] args) {
+        String url = "https://official-joke-api.appspot.com/random_joke";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body)
+                .thenAccept(data -> {
+                    String[] lines = parseResponse(data);
+
+                    System.out.println("Joke: " + lines[0]);
+                    System.out.println("Punch Line: " + lines[1]);
+                }).join();
+    }
+
+    public static String[] parseResponse(String data) {
+        int indexOfSetup = data.indexOf("\"setup\"", 0);
+        String parsableString = data.substring(indexOfSetup, data.length() - 1);
+
+        int indexOfSetupComma = parsableString.indexOf(",");
+        String setup = parsableString.substring(0, indexOfSetupComma);
+        String punch = parsableString.substring(indexOfSetupComma + 1, parsableString.length());
+
+        int indexOfFirstComma = setup.indexOf(":\"");
+        String actualSetupLine = setup.substring(indexOfFirstComma + 2, setup.length() - 1);
+
+        int indexOfSecondComma = punch.indexOf(":\"");
+        String actualPunchLine = punch.substring(indexOfSecondComma + 2, punch.length() - 1);
+
+        String[] lines = { actualSetupLine, actualPunchLine };
+
+        return lines;
+    }
+}
---
+++ index.java
@@ -0,0 +1,29 @@
+public class index {
+    public static void main(String[] arg) {
+        // System.out.println(test(2, 3));
+        // Integer result = test(2,3);
+        // print();
+
+        // String s = new String("Izaan");
+        // String s2 = s;
+        // s = "faraz";
+        // String s3 = "as";
+
+        // System.out.println(s);
+        // System.out.println(s2);
+        // System.out.println(s3.toUpperCase());
+
+        // double s = (double) 1;
+        // System.out.println(s);
+
+    }
+
+    // public static void print() {
+    // Integer x = 10;
+    // System.out.println(x);
+    // }
+
+    // public static Integer test(Integer x, Integer y) {
+    // return x + y;
+    // }
+}
